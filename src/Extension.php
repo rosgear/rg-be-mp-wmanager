55a358c45dbf5ec2efe07b369d3a45cc4f4898db
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 23:00:56 +0300

    Extension

---
+++ src/Extension.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Расширение модуля веб-приложения RosGear.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+namespace Rg\Backend\Marketplace\WidgetManager;
+
+/**
+ * Расширение "Менеджер виджетов сайта".
+ * 
+ * Расширение принадлежит модулю "Маркетплейс".
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Rg\Backend\Marketplace\WidgetManager
+ * @since 1.0
+ */
+class Extension extends \Ge\Panel\Extension\Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public string $id = 'rg.be.mp.wmanager';
+
+    /**
+     * {@inheritdoc}
+     */
+    public string $defaultController = 'grid';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function controllerMap(): array
+    {
+        return [
+            'winfo'     => 'WidgetInfo',
+            'wsettings' => 'WidgetSettings',
+            'hsettings' => 'ShortcodeSettings'
+        ];
+    }
+}
