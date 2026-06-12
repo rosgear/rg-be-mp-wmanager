c625513875da7b58050b8c1c68e8bacade4bec0b
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-12 23:01:28 +0300

    Файл конфигурации версии расширения

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии расширения.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'Widget Manager',
+    'description' => 'Website Widget Manager',
+    'version'     => '1.0',
+    'versionDate' => '20-12-2017',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-be-mp-wmanager/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
